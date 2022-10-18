56b6899a00b670ede314c4829e744dedf8b2504b
Jhosep <jose-zromero@hotmail.com>
2022-10-18 18:53:58 -0500

    Add linters

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,34 @@
-  const path = require('path');
-  const HtmlWebpackPlugin = require('html-webpack-plugin');
-  
-  module.exports = {
-      
-      devServer: {
-          static: './dist',
-        },
-        optimization: {
-          runtimeChunk: 'single',
-        },
-    entry: './src/index.js',
-  
-    plugins: [
-      new HtmlWebpackPlugin({
-          template: './src/index.html',
-      }),
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+
+  devServer: {
+    static: './dist',
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  entry: './src/index.js',
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
-    output: {
-      filename: '[name].js',
-      path: path.resolve(__dirname, 'dist'),
-      clean: true,
-      
-    },
-    module: {
-      rules: [
-        {
-          test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
-        },
-      ],
-    },
-    mode: 'development'
-  };
+  },
+  mode: 'development',
+};
