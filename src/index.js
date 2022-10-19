635c751bc701c4073048985601c7952970625e77
Jhosep <jose-zromero@hotmail.com>
2022-10-19 10:26:41 -0500

    Add structure html|css|js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import _ from 'lodash';
+import lielement from './modules/data';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
 
-  // Lodash,
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-  return element;
-}
 
-document.body.appendChild(component());
+
+
+let uldata = document.querySelector('.scores')
+
+uldata.innerHTML += lielement.generateElement("carlos",100)
+uldata.innerHTML += lielement.generateElement("patrick",300)
+uldata.innerHTML += lielement.generateElement("julian",10)
+uldata.innerHTML += lielement.generateElement("david",1005)
+uldata.innerHTML += lielement.generateElement("juan manuel",6100)
+uldata.innerHTML += lielement.generateElement("lucy",108)
+uldata.innerHTML += lielement.generateElement("carla",10560)
+
+
---
+++ src/modules/data.js
@@ -0,0 +1,8 @@
+export default class lielement{
+
+
+   static generateElement(name,score){ 
+    return `<li>${name}: ${score}</li>`
+}
+}
+
