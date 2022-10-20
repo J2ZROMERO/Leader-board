6c998f8f245203c42b8b301b664533695300cb85
Jhosep <jose-zromero@hotmail.com>
2022-10-20 13:43:44 -0500

    Apply linters

--- src/index.js
+++ src/index.js
@@ -1,35 +1,28 @@
-import lielement from './modules/element';
 import './style.css';
-import request from './modules/request.js'
+import Requestapi from './modules/request';
 
-let api  = new request;
+const api = new Requestapi();
 
+document.getElementById('submit').addEventListener('submit', (e) => {
+  api.postgame('futboll');
 
-document.getElementById('submit').addEventListener('submit',(e)=>{
-   
-    api.postgame("futboll")
+  const name = document.querySelector('.nameS');
+  const score = document.querySelector('.scoreS');
 
-    const name = document.querySelector('.nameS');
-    const score = document.querySelector('.scoreS');
-    
-    if(!(name.value === "" && score.value === "")){
-        api.postscores(name.value,score.value)
-        e.preventDefault()
-        name.value = ""
-        score.value = ""
-    }
-    
-    e.preventDefault()
-    
-    
-})
-  
-  document.querySelector('.refreshB').addEventListener('click', ()=>{
-    api.addlielement()
-  })
-  
-   window.addEventListener('load', ()=>{
-  api.addlielement()
-  
-  })
-  
+  if (!(name.value === '' && score.value === '')) {
+    api.postscores(name.value, score.value);
+    e.preventDefault();
+    name.value = '';
+    score.value = '';
+  }
+
+  e.preventDefault();
+});
+
+document.querySelector('.refreshB').addEventListener('click', () => {
+  api.addlielement();
+});
+
+window.addEventListener('load', () => {
+  api.addlielement();
+});
--- src/modules/request.js
+++ src/modules/request.js
@@ -1,50 +1,35 @@
-//import fetch from 'node-fetch';
-import lielement from './element.js'
- 
- export default class requestapi{
+import lielement from './element';
 
-    postgame =  async (game)  =>{
-    await  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/',{
+export default class Requestapi {
+    postgame = async (game) => {
+      await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
         method: 'POST',
-        headers: {      'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({"name": game})
-       })
-    }
-    
-    postscores= async (name,score) =>{
-        await  fetch("https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/600/scores/",{
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({"user": name, "score": score})
-           })
-         
-        }
-     
-    
-    
-    getscores = async () =>{
-      const response = await  fetch("https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/600/scores/")
-    let data = await response.json()
-    
-    return data
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ name: game }),
+      });
     }
 
-
-  addlielement = async () =>{
-      const data = await this.getscores();
-      let elementsli = "";
-      for (const e of data.result){
-        elementsli += lielement.generateElement(e.user,e.score)
-        }
-        document.querySelector('.scores').innerHTML = elementsli
+    postscores= async (name, score) => {
+      await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/600/scores/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ user: name, score }),
+      });
     }
+
+    getscores = async () => {
+      const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/600/scores/');
+      const data = await response.json();
+
+      return data;
+    }
+
+  addlielement = async () => {
+    const data = await this.getscores();
+    let elementsli = '';
+    for (const e of data.result) {
+      elementsli += lielement.generateElement(e.user, e.score);
+    }
+    document.querySelector('.scores').innerHTML = elementsli;
+  }
 }
-
-
-
-
-
-
-
